c2034aec1d04b1fb6854969843577ca0a6614ffa
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-10-28 19:40:14 +0100

    Github action, dockerfiles

--- FE/tda25-fe/src/app/page.tsx
+++ FE/tda25-fe/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-dosis-bold)] text-4xl">
