c9b9d64682c5ebf18a7b1210a6f7f6d23d7fe66b
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-10-28 11:30:18 +0100

    Initial commit

---
+++ BE/TdA25-BE/src/main/java/com/tda25be/tda25be/TdA25BeApplication.java
@@ -0,0 +1,13 @@
+package com.tda25be.tda25be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdA25BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdA25BeApplication.class, args);
+	}
+
+}
---
+++ BE/TdA25-BE/src/test/java/com/tda25be/tda25be/TdA25BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tda25be.tda25be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdA25BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
