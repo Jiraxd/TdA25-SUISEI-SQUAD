6dc8441916b9af910d120069ba7ac7162388ab6a
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2025-02-09 13:18:56 +0100

    Add online page

---
+++ FE/tda25-fe/src/app/online/page.tsx
@@ -0,0 +1,7 @@
+export default function OnlinePage() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-4 font-dosis-bold text-4xl text-black">
+      <h1>Online Page</h1>
+    </div>
+  );
+}
