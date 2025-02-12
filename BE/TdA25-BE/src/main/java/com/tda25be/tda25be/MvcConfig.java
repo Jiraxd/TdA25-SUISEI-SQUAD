57189b62037f14b0216156270d929eaf73891ca2
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2025-02-12 20:01:09 +0100

    Update MvcConfig.java

--- BE/TdA25-BE/src/main/java/com/tda25be/tda25be/MvcConfig.java
+++ BE/TdA25-BE/src/main/java/com/tda25be/tda25be/MvcConfig.java
@@ -12,17 +12,22 @@ public class MvcConfig implements WebMvcConfigurer {
         // Kvůli limitacím v odevzdávání (nepřišel jsem na způsob, jak nahrát dva funkční Docker image)
         // a našemu výběru tech-stacku jsme se rozhodli využít backend pro forwardování requestů.
 
-        registerRoute(registry, "/game{slash:.*}", "game.html");
-        registerRoute(registry, "/games{slash:.*}", "games.html");
-        registerRoute(registry, "/edit{slash:.*}", "edit.html");
-        registerRoute(registry, "/online{slash:.*}", "online.html");
-        registerRoute(registry, "/profile{slash:.*}", "profile.html");
-        registerRoute(registry, "/onlineGame/{id:.*}", "onlineGame.html");
-        registerRoute(registry, "/login{slash:.*}", "login.html");
-        registerRoute(registry, "/register{slash:.*}", "register.html");
+        registerRoutes(registry, "game");
+        registerRoutes(registry, "games");
+        registerRoutes(registry, "edit");
+        registerRoutes(registry, "online");
+        registerRoutes(registry, "profile");
+        registerRoutes(registry, "login");
+        registerRoutes(registry, "register");
+
+        // Special cases with dynamic IDs
+        registry.addViewController("/game/{id:.*}").setViewName("forward:/game.html");
+        registry.addViewController("/profile/{id:.*}").setViewName("forward:/profile.html");
+        registry.addViewController("/onlineGame/{id:.*}").setViewName("forward:/onlineGame.html");
     }
 
-    private void registerRoute(ViewControllerRegistry registry, String path, String view) {
-        registry.addViewController(path).setViewName("forward:/" + view);
+    private void registerRoutes(ViewControllerRegistry registry, String route) {
+        registry.addViewController("/" + route).setViewName("forward:/" + route + ".html");
+        registry.addViewController("/" + route + "/").setViewName("forward:/" + route + ".html");
     }
 }
