c6a904b573f31dfc48d3388d0d31ea2bca1c89dd
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2025-02-19 19:45:26 +0100

    Rank color changes

--- FE/tda25-fe/src/models/Rank.tsx
+++ FE/tda25-fe/src/models/Rank.tsx
@@ -6,15 +6,15 @@ export type Rank = {
 };
 
 export const RANKS: Rank[] = [
-  { name: "UNRANKED", minElo: 0, maxElo: 0, color: "#6B7280" }, // Gray
-  { name: "IRON", minElo: 1, maxElo: 299, color: "#8B8C89" }, // Iron Gray
-  { name: "BRONZE", minElo: 300, maxElo: 599, color: "#CD7F32" }, // Bronze
-  { name: "SILVER", minElo: 600, maxElo: 899, color: "#C0C0C0" }, // Silver
-  { name: "GOLD", minElo: 900, maxElo: 1199, color: "#FFD700" }, // Gold
-  { name: "PLATINUM", minElo: 1200, maxElo: 1499, color: "#A8C3BC" }, // Platinum
-  { name: "DIAMOND", minElo: 1500, maxElo: 1799, color: "#B9F2FF" }, // Diamond Blue
-  { name: "MASTER", minElo: 1800, maxElo: 1999, color: "#FF4069" }, // Master
-  { name: "GRANDMASTER", minElo: 2000, maxElo: Infinity, color: "#FF0000" }, // Grandmaster
+  { name: "UNRANKED", minElo: 0, maxElo: 0, color: "#6B7280" },
+  { name: "IRON", minElo: 1, maxElo: 299, color: "#8B8C89" },
+  { name: "BRONZE", minElo: 300, maxElo: 599, color: "#b06d2a" },
+  { name: "SILVER", minElo: 600, maxElo: 899, color: "#C0C0C0" },
+  { name: "GOLD", minElo: 900, maxElo: 1199, color: "#cbaf18" },
+  { name: "PLATINUM", minElo: 1200, maxElo: 1499, color: "#57b89f" },
+  { name: "DIAMOND", minElo: 1500, maxElo: 1799, color: "#328bc5" },
+  { name: "MASTER", minElo: 1800, maxElo: 1999, color: "#FF4069" },
+  { name: "GRANDMASTER", minElo: 2000, maxElo: Infinity, color: "#ca1c6b" },
 ];
 
 export function getRankByElo(elo: number): Rank {
