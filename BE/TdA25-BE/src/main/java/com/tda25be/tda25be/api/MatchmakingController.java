ffe3a89822cf4b270e6b88d8f483b08f7a1e02a4
Ondřej Lenikus <ondralenikus1@seznam.cz>
2025-02-20 20:09:53 +0100

    Matchmaking fix

--- BE/TdA25-BE/src/main/java/com/tda25be/tda25be/api/MatchmakingController.java
+++ BE/TdA25-BE/src/main/java/com/tda25be/tda25be/api/MatchmakingController.java
@@ -1,6 +1,8 @@
 package com.tda25be.tda25be.api;
 
+import com.tda25be.tda25be.entities.Session;
 import com.tda25be.tda25be.entities.User;
+import com.tda25be.tda25be.repositories.SessionRepo;
 import com.tda25be.tda25be.repositories.UserRepo;
 import com.tda25be.tda25be.services.matchmaking.MatchmakingService;
 import com.tda25be.tda25be.services.matchmaking.MatchmakingTypes;
@@ -20,19 +22,18 @@ public class MatchmakingController {
     private final List<MatchmakingService> matchmakingServices;
     private final UserRepo userRepo;
     private final MatchmakingService matchmakingService;
+    private final SessionRepo sessionRepo;
 
     @GetMapping("/start")
     public ResponseEntity<String> startMatchmaking(@RequestParam MatchmakingTypes matchmaking , @RequestHeader("Authorization") String token){
         if(matchmaking == null || token == null) return ResponseEntity.badRequest().build();
-        User user = userRepo.findById(token).orElse(null);
-        if(user == null) return ResponseEntity.notFound().build();
+        Session session = sessionRepo.findByToken(token).orElse(null);
+        if(session == null || session.getUser() == null) return ResponseEntity.badRequest().build();
+        User user = session.getUser();
         MatchmakingService matchmakingService = matchmakingServices.stream().filter((service)->{
             if(matchmaking == MatchmakingTypes.ranked && service instanceof RankedMatchmakingService){
                 return true;
-            } else if (matchmaking == MatchmakingTypes.unranked && service instanceof UnrankedMatchmakingService) {
-                return true;
-            }
-            return false;
+            } else return matchmaking == MatchmakingTypes.unranked && service instanceof UnrankedMatchmakingService;
         }).findFirst().get();
         matchmakingService.joinMatchmaking(user);
         matchmakingService.matchmake(user);
@@ -41,8 +42,9 @@ public class MatchmakingController {
 
     @GetMapping("/cancel")
     public ResponseEntity<String> cancelMatchmaking(@RequestHeader("Authorization") String token){
-        User user = userRepo.findById(token).orElse(null);
-        if(user == null) return ResponseEntity.notFound().build();
+        Session session = sessionRepo.findByToken(token).orElse(null);
+        if(session == null || session.getUser() == null) return ResponseEntity.badRequest().build();
+        User user = session.getUser();
         matchmakingService.leaveMatchmaking(user);
         return ResponseEntity.ok("Matchmaking stopped");
     }
